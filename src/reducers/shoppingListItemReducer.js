9102855cb7865f67b181728a38aecbddc539dfc4
Autonomoustic <aa_connect@outlook.com>
2018-11-17 20:54:46 +0000

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,17 @@
-export default function shoppingListItemReducer(
+export default function shoppingListItemReducer (
 	state = {
-		items: []
-	},
+  items: []
+},
 	action
 ) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      console.log('current state.items length %s', state.items.length)
+      console.log('updating state.items length to %s', state.items.length + 1)
+      return state.items.concat(state.items.length + 1)
 
-		default:
-			return state;
-	}
+    default:
+      console.log('initial state.items length: %s', state.items.length)
+      return state
+  }
 }
